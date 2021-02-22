54d6ba8c1291e01fee175982cb496ef4ce340a45
Erik Seyti <erikseyti@gmail.com>
2021-02-22 19:30:17 -0300

    Examples from oreily to use multithread in Python

---
+++ multi_thread-ex1.py
@@ -0,0 +1,32 @@
+import requests
+websites = [
+    'http://en.kremlin.ru/',
+    'http://mfa.go.th/main/',
+    'http://www.mofa.gov.la/',
+    'http://www.presidency.gov.gh/',
+    'https://www.aph.gov.au/',
+    'https://www.argentina.gob.ar/',
+    'https://www.fmprc.gov.cn/mfa_eng/',
+    'https://www.gcis.gov.za/',
+    'https://www.gov.ro/en',
+    'https://www.government.se/',
+    'https://www.india.gov.in/',
+    'https://www.jpf.go.jp/e/',
+    'https://www.oreilly.com/',
+    'https://www.parliament.nz/en/',
+    'https://www.peru.gob.pe/',
+    'https://www.premier.gov.pl/en.html',
+    'https://www.saskatchewan.ca/'
+]
+
+def visit_website(url):
+    """
+    Makes a GET request to a website URL and prints response information
+    """
+    r = requests.get(url)
+    print(f'{url} returned to {r.status_code} after {r.elapsed} seconds')
+if __name__ == '__main__':
+    print('Main thread started')
+    for website in websites:
+        visit_website(website)
+    print('Main thread ended')
